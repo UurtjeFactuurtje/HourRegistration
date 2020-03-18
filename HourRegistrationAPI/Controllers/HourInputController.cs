7125173c8b753f9c2703b386aa1992867af304cd
RoyTimmers <roytimmers94@gmail.com>
2020-03-18 21:38:10 +0100

    Initial copy

---
+++ HourRegistrationAPI/Controllers/HourInputController.cs
@@ -0,0 +1,34 @@
+﻿using HourRegistrationAPI.MessageProducer;
+using HourRegistrationAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace HourRegistrationAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HourInputController : ControllerBase
+    {
+        HourRegistrationProducer producer = new HourRegistrationProducer();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> PostHours()
+        {
+            return new string[] { "It works" };
+        }
+
+        //[HttpPost]
+        //public async Task<ActionResult<HourRegistrationModel>> RegisterHours(HourRegistrationModel input)
+        //{
+        //    producer.ProduceHourRegistrationMessage(input);
+
+        //    return CreatedAtAction(nameof(input), new { id = input.id }, input);
+        //}
+
+        [HttpPost]
+        public void RegisterHours(HourRegistrationModel input)
+        {
+            producer.ProduceHourRegistrationMessage(input);
+        }
+    }
+}
